b4d3b3a6466cc0638dfcbee16117fa94cea93a47
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-02 00:21:16 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
 
-attr_accessor :firstname, :lastname
+attr_accessor :first_name, :last_name
 
-def initialize(firstname, lastname)
-  @firstname = firstname
-  @lastname = lastname
+def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
 end
 
 end
